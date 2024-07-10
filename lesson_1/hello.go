356ea43b22fa9575c2095ba7284aefbd40503f18
saiddis <saiddisruptive@gmail.com>
2024-07-10 19:40:32 +0500

    first commit

---
+++ lesson_1/hello.go
@@ -0,0 +1,46 @@
+package main
+
+func main() {
+	/* println("Hello World!") */
+	/* var num int
+	num = 7
+	fmt.Println(num)
+
+	num = 8
+	fmt.Println(num) */
+
+	/* num3 := 3
+	fmt.Println(num3)
+
+	var num4 int8 = 120
+	fmt.Println(num4)
+
+	var boolean bool = false
+	fmt.Println(boolean) */
+
+	/* var str string = "hello"
+	str2 := str + num3  */
+
+	/* var num3 = 9
+
+	var num4 int8 = 8
+	num5 := int8(num3) + num4
+	fmt.Println(num5) */
+
+	/* var ex int = 10
+	var b2 byte
+	var r rune
+	var ex2 = ex
+	fmt.Println(ex2)
+
+	const num = 1e99
+	fmt.Println(num)
+
+	const (
+		n1 = 0 == iota
+		n2 = 1
+		n3 = 2
+	)
+
+	fmt.Println(n2) */
+}
